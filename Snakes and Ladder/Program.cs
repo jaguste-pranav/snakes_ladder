70aad4d29e5da6c9cd28e51d079a99217b2df18a
jaguste.pranav <jaguste.pranav31@gmail.com>
2020-09-29 01:11:58 +0530

    [pranav] Add. Welcome to Snakes and Ladder Solution.

---
+++ Snakes and Ladder/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Snakes_and_Ladder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
