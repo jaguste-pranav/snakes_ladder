cfb7ef4863db9a46eca2e9446b9354687c9c406a
jaguste.pranav <jaguste.pranav31@gmail.com>
2020-09-29 01:32:01 +0530

    [pranav] Add. Simulate 2 players playing

--- Snakes and Ladder/Program.cs
+++ Snakes and Ladder/Program.cs
@@ -1,6 +1,4 @@
-﻿
-using System;
-using System.Runtime.InteropServices;
+﻿using System;
 
 namespace Snakes_and_Ladder
 {
@@ -26,57 +24,103 @@ namespace Snakes_and_Ladder
 
         static void Main(string[] args)
         {
-            int currentPosition = 0;
+            int currentPositionPlayer1 = 0;
+            int currentPositionPlayer2 = 0;
             int SNAKE = 0;
             int NO_PLAY = 1;
             int LADDER = 2;
             int diceCounter = 0;
 
-            while (currentPosition <= 100)
-            //for (int i = 0; i < 500; i++)
+            while (currentPositionPlayer1 <= 100 || currentPositionPlayer2 <= 100)
             {
                 diceCounter++;
-                int dice = diceRoll();
-                int option = optionRoll();
+                int dicePlayer1 = diceRoll();
+                int optionPlayer1 = optionRoll();
 
-                if (option == SNAKE)
+                int dicePlayer2 = diceRoll();
+                int optionPlayer2 = optionRoll();
+
+                if (optionPlayer1 == SNAKE)
                 {
-                    if (currentPosition - dice >= 0)
+                    if (currentPositionPlayer1 - dicePlayer1 >= 0)
                     {
-                        currentPosition = currentPosition - dice;
+                        currentPositionPlayer1 = currentPositionPlayer1 - dicePlayer1;
                     }
                     else
                     {
-                        currentPosition = 0;
+                        currentPositionPlayer1 = 0;
                     }
                 }
 
-                if (option == NO_PLAY)
+                if (optionPlayer1 == NO_PLAY)
                 {
-                    currentPosition = currentPosition + 0;
+                    currentPositionPlayer1 = currentPositionPlayer1 + 0;
                 }
 
-                if (option == LADDER)
+                if (optionPlayer1 == LADDER)
                 {
-                    if (currentPosition + dice < 100)
+                    if (currentPositionPlayer1 + dicePlayer1 < 100)
                     {
-                        currentPosition = currentPosition + dice;
+                        currentPositionPlayer1 = currentPositionPlayer1 + dicePlayer1;
                     }
 
-                    else if (currentPosition + dice > 100)
+                    else if (currentPositionPlayer1 + dicePlayer1 > 100)
                     {
-                        currentPosition = currentPosition + 0;
+                        currentPositionPlayer1 = currentPositionPlayer1 + 0;
                     }
                     else
                     {
-                        currentPosition = currentPosition + dice;
+                        currentPositionPlayer1 = currentPositionPlayer1 + dicePlayer1;
+                        Console.WriteLine("Player 1 has won!!");
                         break;
                     }
                 }
-                Console.WriteLine("The final position is " + currentPosition);
+                Console.WriteLine("The final position for player 1 is " + currentPositionPlayer1);
+
+                if (optionPlayer2 == SNAKE)
+                {
+                    if (currentPositionPlayer2 - dicePlayer2 >= 0)
+                    {
+                        currentPositionPlayer2 = currentPositionPlayer2 - dicePlayer2;
+                    }
+                    else
+                    {
+                        currentPositionPlayer2 = 0;
+                    }
+                }
+
+                if (optionPlayer2 == NO_PLAY)
+                {
+                    currentPositionPlayer2 = currentPositionPlayer2 + 0;
+                }
+
+                if (optionPlayer2 == LADDER)
+                {
+                    if (currentPositionPlayer2 + dicePlayer2 < 100)
+                    {
+                        currentPositionPlayer2 = currentPositionPlayer2 + dicePlayer2;
+                    }
+
+                    else if (currentPositionPlayer2 + dicePlayer2 > 100)
+                    {
+                        currentPositionPlayer2 = currentPositionPlayer2 + 0;
+                    }
+
+                    else
+                    {
+                        currentPositionPlayer2 = currentPositionPlayer2 + dicePlayer2;
+                        Console.WriteLine("Player 2 has won!!");
+                        break;
+                    }
+
+                }
+                Console.WriteLine("The final position for player 2 is " + currentPositionPlayer2);
                 Console.WriteLine("");
+
             }
             Console.WriteLine("The dice was rolled " + diceCounter + " number of times");
+
+
         }
     }
 }
